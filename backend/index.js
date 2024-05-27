d79c1638b6270783b208831dc4131b84c745da4b
MinhHo <minh.ho89@gmail.com>
2024-05-27 22:33:39 +0700

    update gitignore and initialize the backend, including POST GET API for PRODUCTS

---
+++ backend/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const uuid = require("uuid");
+
+const app = express();
+
+const DUMMY_PRODUCTS = [];
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+app.get('/products', (req, res, next) => {
+    res.status(200).json({products: DUMMY_PRODUCTS});
+});
+app.post('/product', (req, res, next) => {
+    const {title, price} = req.body;
+
+    if (!title || title.trim().length === 0 || !price || price <= 0) {
+        return res.status(422).json({message: 'Invalid input, please enter a valid title and price.'});
+    }
+
+    const createdProduct = {
+        id: uuid.v4(),
+        title,
+        price
+    };
+
+    DUMMY_PRODUCTS.push(createdProduct);
+
+    res.status(201).json({message: 'Created new product.', product: createdProduct});
+
+});
+
+
+app.listen(5002, () => {
+    console.log('Server started on port 5002');
+});
