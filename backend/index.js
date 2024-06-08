1d5b8e02535e52be1cd969ecd5ae0ecf5a9c0ff0
MinhHo <minh.ho89@gmail.com>
2024-06-08 11:57:59 +0700

    Integrate socket for both ends on new product update

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,35 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const uuid = require("uuid");
 const cors = require('cors');
-
+const http = require("http");
+const socketIo = require("socket.io");
+const {createServer} = require("http");
+const { Server } = require('socket.io');
 const app = express();
+const server = createServer(app)
+
+const io = new Server(server, {
+    cors: {
+        origin: ["http://localhost:3000", "http://localhost:3001"],
+        method: ["GET", "POST"]
+    }
+});
+
+io.on('connection', (socket)=> {
+   console.log('a user connected with id: ', socket.id) ;
+});
+
+// server.listen(5002, () => {
+//     console.log('listening on *:5002');
+// })
+
 require('dotenv').config();
 
 const DUMMY_PRODUCTS = [];
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.use(cors({ origin: 'http://localhost:3000'}));
+app.use(cors({ origin: ['http://localhost:3000', "http://localhost:3001"]}));
 
 
 app.get('/products', (req, res) => {
@@ -31,11 +51,13 @@ app.post('/product', (req, res) => {
 
     DUMMY_PRODUCTS.push(createdProduct);
 
+    io.emit('new-product', createdProduct);
+
     res.status(201).json({message: 'Created new product.', product: createdProduct});
 
 });
 
 const port = process.env.PORT || 5002;
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
--- frontend/src/components/ProductList/ProductList.jsx
+++ frontend/src/components/ProductList/ProductList.jsx
@@ -1,12 +1,22 @@
 import React, {useEffect, useState} from "react";
+import socketIOClient from "socket.io-client";
 import API from "../../services/api";
 import ProductCard from "../ProductCard/ProductCard";
 import NoProduct from "../NoProductMessage/NoProduct";
 
 const ProductList = () => {
     const [products, setProducts] = useState([]);
+    const [socket, setSocket] = useState(null);
 
     useEffect(() => {
+        const newSocket = socketIOClient('http://localhost:5002');
+        setSocket(newSocket);
+        return () => newSocket.close();
+    }, []);
+
+    useEffect(() => {
+        if (!socket) return;
+
         const fetchProducts = async () => {
             try {
                 const data = await API.getProducts();
@@ -15,8 +25,18 @@ const ProductList = () => {
                 alert(error);
             }
         };
-        fetchProducts()
-    },[]);
+        fetchProducts();
+
+        socket.on('new-product', (newProduct) => {
+           setProducts((prevProducts) => [...prevProducts, newProduct]) ;
+        });
+
+        return () => {
+            if (socket) {
+                socket.off('new-product');
+            }
+        };
+    },[socket]);
 
     return (
        <>
